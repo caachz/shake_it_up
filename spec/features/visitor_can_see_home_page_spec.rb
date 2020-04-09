94e920abbd7a6afc19dd1f4795cd684fcec64331
Alex Gallant <agallant1201@gmail.com>
2020-04-09 17:07:15 -0600

    Add spec to see home page
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>

--- spec/features/visitor_can_see_home_page_spec.rb
+++ spec/features/visitor_can_see_home_page_spec.rb
@@ -6,3 +6,6 @@ RSpec.describe 'As a visitor' do
     expect(page).to have_button('Login with Google')
   end
 end
+
+client id 203691594435-t659jgqg8lfmbjkt17sbrhk1kv24m4g2.apps.googleusercontent.com
+client secret 8cDO92aXKPUhtggtA9Mp4zzd
