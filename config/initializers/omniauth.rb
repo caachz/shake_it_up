e04c2239e9e5d252d43d2090561a73cec116874e
Alex Gallant <agallant1201@gmail.com>
2020-04-09 17:08:17 -0600

    Add omniauth initializer
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV["CLIENT_ID"], ENV["CLIENT_SECRET"]
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+end
