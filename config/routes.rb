7c433180047a23a168efb3c8de4123a1fc285bff
Alex Gallant <agallant1201@gmail.com>
2020-04-09 17:07:26 -0600

    Add route for google oauth
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'welcome#index'
+  root to: 'welcome#index'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_scope :user do
+  #   get 'users/sign_in', to: 'users/sessions#new', as: :new_user_session
+  #   get 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+  # end
 end
