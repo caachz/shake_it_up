4a0a8054656c0da08727a99e5b342d01ccf87f77
Alex Gallant <agallant1201@gmail.com>
2020-04-11 16:55:23 -0600

    Update initializer with client id and secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET']
+  provider :google_oauth2, "797986626675-l7lao223kk3kmvdu2en3peb4h6rhinko.apps.googleusercontent.com", "F4jOq5CNovvOJ_VqLgBprkhB"
 end
