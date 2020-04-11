ae9b720499d2dd71bf52509b8957262fad32138e
Alex Gallant <agallant1201@gmail.com>
2020-04-11 17:49:46 -0600

    Edit routes in redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,8 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(env["omniauth.auth"])
+    @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
     session[:user_id] = @user.id
-    redirect_to :me
+    redirect_to me_path
   end
 
   def destroy
