244ba88155405aa3a3bac8bfaf3833c6b90bdb9a
Alex Gallant <agallant1201@gmail.com>
2020-04-12 16:05:33 -0600

    Rename me routes to dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get 'logout', to: 'sessions#destroy', as: 'logout'
 get 'auth/:provider/callback', to: 'sessions#create'
 get 'auth/failure', to: redirect('/')
 get 'home', to: 'home#show'
-get 'me', to: 'me#show', as: 'me'
+get '/dashboard', to: 'dashboard#show', as: 'dashboard'
 #change me to dashboard at some point and can be the page the user uses to shake it up
 root to: "home#show"
 end
