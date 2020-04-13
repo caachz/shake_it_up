4f4abcae901a9da3a08b263f50eb0a368ae40d0b
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-04-13 10:59:09 -0600

    merge related changes

--- spec/features/visitor_can_see_home_page_spec.rb
+++ spec/features/visitor_can_see_home_page_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe 'As a visitor' do
   it "can visit the homepage as a visitor" do
     visit '/'
-    expect(page).to have_button('Login with Google')
+    expect(page).to have_link('Login with Google')
   end
 end
