884345aeb72f7b9a69710a1e8b142e49c9f87e82
Alex Gallant <agallant1201@gmail.com>
2020-04-14 17:10:22 -0600

    Edit spec to show drop down menus
    Co-Authored-By: Cassie <caachz@users.noreply.github.com>
    Co-Authored-By: Matthew Holmes <holmesm8@gmail.com>

--- spec/features/user_sees_dashboard_functionality_when_signed_in_spec.rb
+++ spec/features/user_sees_dashboard_functionality_when_signed_in_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe 'As a logged in user' do
   before :each do
     visit root_path
     stub_omniauth
-    click_link 'Sign in with Google'
+    click_button 'Sign in with Google'
 
     expect(current_path).to eq(dashboard_path)
     expect(page).to have_content('Happy')
-    # expect(page).to have_content('Gilmore')
   end
 
-  xit "shows options for users via dropdown menus before they shake it up" do
-    expect(page).to have_content('Welcome, Happy Gilmore')
+  it "shows options for users via dropdown menus before they shake it up" do
+    expect(page).to have_content('Welcome, Happy')
     expect(page).to have_content('Logout')
     select "1 Mile", from: :distance
-    select "$$$", from: :prices
+    select "$$$", from: :price
     click_button 'Shake It Up!'
   end
 end
