43bff3924fac57722a80593ad6226200e6f4dda5
Alex Gallant <agallant1201@gmail.com>
2020-04-15 09:33:46 -0600

    Add flash message functionality
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>
    Co-Authored-By: Cassie <caachz@users.noreply.github.com>
    Co-Authored-By: Matthew Holmes <holmesm8@gmail.com>

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -3,6 +3,12 @@ class RestaurantController < ApplicationController
   def show
     service = RevItUpService.new(current_user.lat, current_user.lng, meters_to_miles, params["price"])
     @restaurant = RevItUpFacade.new(service.restaurant)
+    if service.restaurant == "error"
+      flash[:notice] = "No restaurants meet this criteria, please try again!"
+      redirect_to dashboard_path
+    else
+      @restaurant = RevItUpFacade.new(service.restaurant)
+    end
   end
 
   def meters_to_miles
