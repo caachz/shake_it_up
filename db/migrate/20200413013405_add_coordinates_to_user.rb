40722ed185e4b622fed8f20d0a29005fd36ff739
Cassie Achzenick <caachzenick@gmail.com>
2020-04-16 20:33:33 -0600

    remove default in migration

--- db/migrate/20200413013405_add_coordinates_to_user.rb
+++ db/migrate/20200413013405_add_coordinates_to_user.rb
@@ -1,6 +1,6 @@
 class AddCoordinatesToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :lat, :float, default: GoogleGeolocationService.new.coordinates[:location][:lat]
-    add_column :users, :lng, :float, default: GoogleGeolocationService.new.coordinates[:location][:lng]
+    add_column :users, :lat, :float
+    add_column :users, :lng, :float
   end
 end
